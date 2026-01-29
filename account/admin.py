6daf3e55cf32a4ef4909b777aef708742b4d6d6d
dera1992 <30830532+dera1992@users.noreply.github.com>
2026-01-29 09:10:51 +0000

    Add admin user forms for account

--- account/admin.py
+++ account/admin.py
@@ -2,21 +2,24 @@ from django.contrib import admin
 from .models import Profile
 
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import AdminUserChangeForm, AdminUserCreationForm
 from .models import User
 
 
 class UserAdmin(BaseUserAdmin):
+    form = AdminUserChangeForm
+    add_form = AdminUserCreationForm
     list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number', 'role')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number', 'role', 'is_staff', 'is_active')}
          ),
     )
     search_fields = ('email',)
