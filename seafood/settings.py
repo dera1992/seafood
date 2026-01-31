6b84d2c50bdb6c12a2affb38891c1f820b06926c
Eze Chidera <ezechdr16@gmail.com>
2026-01-31 09:16:40 +0000

    changes done on the email setting

--- seafood/settings.py
+++ seafood/settings.py
@@ -15,6 +15,7 @@ from ctypes.util import find_library
 
 import environ
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -245,9 +246,11 @@ HITCOUNT_HITS_PER_IP_LIMIT = 0
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ezechdr16@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = f"Bunchfood <{EMAIL_HOST_USER}>"
 
 
 STAR_RATINGS_STAR_HEIGHT = 22
