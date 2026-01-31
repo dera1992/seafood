25a12be469fa6e25a0526ca636dfd90e0cb97c72
dera1992 <30830532+dera1992@users.noreply.github.com>
2026-01-31 07:11:45 +0000

    Add dispatcher step navigation links

--- seafood/settings.py
+++ seafood/settings.py
@@ -140,6 +140,20 @@ AUTHENTICATION_BACKENDS = (
 
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'account.social_pipeline.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
